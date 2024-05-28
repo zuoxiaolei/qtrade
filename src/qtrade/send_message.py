43ec641276a2bc14868cbe8f7c9f530230751870
zuoxiaolei <1905561110@qq.com>
2024-05-28 16:30:41 +0800

    fix send message

--- src/qtrade/send_message.py
+++ src/qtrade/send_message.py
@@ -1,6 +1,7 @@
 import requests
 import pytz
 from datetime import datetime
+from mysql_util import get_connection
 
 tokens = [
     'b0b21688d4694f7999c301386ee90a0c',  # xiaolei
@@ -10,11 +11,11 @@ tz = pytz.timezone('Asia/Shanghai')
 now = datetime.now(tz).strftime("%Y-%m-%d")
 
 
-def send_message(content):
+def send_message(content, title):
     for token in tokens:
         params = {
             'token': token,
-            'title': '组合投资策略',
+            'title': title,
             'content': content,
             'template': 'txt'}
         url = 'http://www.pushplus.plus/send'
@@ -26,10 +27,32 @@ def send_ratation_message(last_day, last_day_profit):
     print({"last_day": last_day, "now": now})
     last_day_profit = round(last_day_profit, 2)
     hour = datetime.now(tz).hour
+    
+    with get_connection() as cursor:
+        cursor.execute("""select max(date) from etf.ods_etf_history""")
+        max_date = cursor.fetchall()[0][0]
+    max_month = max_date[:7]
+    max_year = max_date[:4]
+    
+    with get_connection() as cursor:
+        cursor.execute(f'''select value 
+                            from etf.ads_etf_portfolio_profit_summary
+                            where date='{max_month}' ''')
+        increase_month = cursor.fetchall()[0][0]
+        
+    with get_connection() as cursor:
+        cursor.execute(f'''select value 
+                                              from etf.ads_etf_portfolio_profit_summary
+                                               where date='{max_year}' ''')
+        increase_year = cursor.fetchall()[0][0]
+
+    increase_month = round(increase_month, 2)
+    increase_year = round(increase_year, 2)
     if last_day == now and 9 <= hour <= 15:
-        message = f"组合投资\n日期：{last_day}\n涨幅：{last_day_profit}%"
-        send_message(message)
+        message = f"组合投资\n日期：{last_day}\n日涨幅：{last_day_profit}%\n月涨幅：{increase_month}%\n年涨幅：{increase_year}%"
+        send_message(message, title="组合投资")
 
 
 if __name__ == '__main__':
-    pass
+    send_ratation_message("2024-05-28", last_day_profit=1.0)
+    
