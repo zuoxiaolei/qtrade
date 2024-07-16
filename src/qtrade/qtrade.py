83baec2f045ddde3dde51a61c52d66642978d1d6
zuoxiaolei <1905561110@qq.com>
2024-07-16 20:56:19 +0800

    add rank portfolio strategy

--- src/qtrade/qtrade.py
+++ src/qtrade/qtrade.py
@@ -6,17 +6,19 @@ from streamlit_echarts import st_echarts
 import empyrical
 from easyquotation import use
 from rank_strategy_app import rotation_strategy
+from datetime import datetime
+import numpy as np
+pymysql.install_as_MySQLdb()
 
 quotation = use('sina')
 portfolio_code = ['518880', '512890', '159941']
 portfolio_name = ['黄金ETF', '红利ETF', '纳指ETF']
-pymysql.install_as_MySQLdb()
+
 
 is_local = False
 ttl = 600
 height = 740
 width = 800
-# st.set_page_config(layout='wide')
 
 mysql_conn = st.connection('mysql', type='sql', ttl=ttl)
 max_date_sql = '''
@@ -29,10 +31,12 @@ max_date = mysql_conn.query(max_date_sql, ttl=ttl).values.tolist()[0][0]
 def get_portfolio_realtime_data(stock_code):
     realtime_dict = quotation.stocks(stock_code[1:])
     all_increase_rate = [0] * len(stock_code)
+    date_str = None
     for num, code in enumerate(stock_code):
         if num > 0:
             now = realtime_dict[code]['now']
             close = realtime_dict[code]['close']
+            date_str = realtime_dict[code]['date']
             increase_rate = round((now / close - 1) * 100, 2)
             all_increase_rate[num] = increase_rate
 
@@ -41,7 +45,7 @@ def get_portfolio_realtime_data(stock_code):
                             0.2 * all_increase_rate[3]
                             )
     all_increase_rate[0] = round(all_increase_rate[0], 2)
-    return all_increase_rate
+    return all_increase_rate, date_str
 
 
 def portfolio_strategy():
@@ -60,7 +64,7 @@ def portfolio_strategy():
     max_month = max_date[:7]
     max_year = max_date[:4]
 
-    all_increase_rate = get_portfolio_realtime_data(['all'] + portfolio_code)
+    all_increase_rate, _ = get_portfolio_realtime_data(['all'] + portfolio_code)
     increase_rate = all_increase_rate[1:]
     increase_month = mysql_conn.query(f'''select value 
                                           from etf.ads_etf_portfolio_profit_summary
@@ -97,7 +101,7 @@ def portfolio_strategy():
     options = ['all'] + options
 
     st.markdown("### 收益曲线")
-    select_year = st.selectbox(label='年份', options=options)
+    select_year = st.selectbox(label='年份', options=options, key=3)
     if select_year != 'all':
         df_portfolio = df_portfolio[df_portfolio.date.map(lambda x: x[:4] == select_year)]
 
@@ -204,48 +208,190 @@ def calc_indicators(df_returns):
     sharpe = empyrical.sharpe_ratio(df_returns)
     return accu_returns, annu_returns, max_drawdown, sharpe
 
+def get_params(df):
+    df = df.sort_values(by="date")
+    df = df.drop_duplicates(subset=["date"])
+    df["c"] = df.close.map(float)
+    df["future_increase_rate1"] = (df.c.shift(-1)/df.c-1)*100
+    
+    df["date"] = df["date"].apply(lambda x: datetime.strptime(x, "%Y-%m-%d"))
+    df["year"] = df["date"].map(lambda x: x.year)
+    mean_rate = df["future_increase_rate1"].mean()
+    
+    def get_rank(series):
+        data = series.values
+        return np.argsort(-data)[-1]
+    
+    for i in range(2, 21):
+        df[f"c_rank{i}"] = df.c.rolling(i).apply(get_rank)
+    
+    # get model parameter
+    feature_score = {}
+    for i in range(2, 21):
+        tmp = df.groupby(f"c_rank{i}", as_index=False)["future_increase_rate1"].mean()
+        tmp = tmp[tmp["future_increase_rate1"]>=mean_rate]
+        feature_score[f"c_rank{i}"] = tmp[f"c_rank{i}"].tolist()
+    model = feature_score
+    keys = list(model.keys())
+    def get_rank_strategy(row):
+        score = 0
+        for ele in keys:
+            if row[ele] in model[ele]:
+                score += 1
+        return score
+    df["buy_sell_label"] = df.apply(get_rank_strategy, axis=1)
+    return df, model
 
-def get_okex_list():
-    sql = '''
-    select t1.date, t1.unique_name, t1.nick_name,
-			 CONCAT('https://www.okx.com/cn/copy-trading/account/', t1.unique_name, '?tab=swap') profile_url,
-			 follow_pnl,
-			 win_rate,
-			 yield_rate,
-			 lever
-from okex.user_info_daily t1
-join okex.user_level t2
-  on t1.unique_name=t2.unique_name
-where date=(SELECT max(date) from okex.user_info_daily) and lever<=10 and follow_pnl>0
-order by t1.follow_pnl desc
+def back_test(is_buy_array, close_array, date_array):
+    profit = 1
+    last_status = 0
+    profits = []
+    
+    for i in range(1, len(close_array)):
+        is_buy = is_buy_array[i]
+        last_price = close_array[i-1]
+        current_price = close_array[i]
+        profit = profit*(current_price/last_price) if last_status else profit
+        profits.append([date_array[i], profit, is_buy])
+        last_status = is_buy
+        
+    df_profits = pd.DataFrame(profits, columns=["date", "rate", "status"])
+    df_profits.index = df_profits["date"]
+    df_profits["increase_rate"] = df_profits["rate"] / df_profits["rate"].shift() - 1
+    sharpe = empyrical.sharpe_ratio(df_profits["increase_rate"])
+    max_drawdown = empyrical.max_drawdown(df_profits["increase_rate"])
+    annual_return = empyrical.annual_return(df_profits["increase_rate"])
+    return profit, sharpe, annual_return, max_drawdown, df_profits
+
+def rank_portfolio_strategy():
+    st.markdown("## 组合择时策略")
+    st.markdown("分数大于等于7买入")
+    all_increase_rate, date_str = get_portfolio_realtime_data(['all'] + portfolio_code)
+    # 筛选时间
+    sql = f'''
+    select date, rate close 
+    from ads_eft_portfolio_rpt t
+    order by date
     '''
-    df_news = mysql_conn.query(sql, ttl=0)
-    df_news.columns = ['日期', '用户标识', '昵称', '个人主页', '收益', '胜率', '回报率', '杠杆倍数']
-    st.markdown("# okex列表")
-    df_news['用户标识'] = df_news.apply(lambda x: f"[{x['用户标识']}]({x['个人主页']})", axis=1)
-    df_news['昵称'] = df_news.apply(lambda x: f"[{x['昵称']}]({x['个人主页']})", axis=1)
-    df_news = df_news.drop("个人主页", axis=1)
-    st.markdown(df_news.to_markdown(index=False), unsafe_allow_html=True)
+    df_portfolio = mysql_conn.query(sql, ttl=0)
+    
+    if date_str!= df_portfolio["date"].max():
+        last_rate = float(df_portfolio.iloc[-1]["close"])
+        current_rate = (all_increase_rate[0]/100+1)*last_rate
+        df_portfolio.append({"date": date_str, "close": current_rate}, ignore_index=True)
+
+    df_portfolio, model = get_params(df_portfolio)
+    df_portfolio["date"] = df_portfolio["date"].map(lambda x: x.strftime("%Y-%m-%d"))
+    st.dataframe(df_portfolio[["date", "close", "buy_sell_label"]].tail(100)[::-1], hide_index=True, width=width, height=200)
+    min_date = df_portfolio.date.min()
+    max_date = df_portfolio.date.max()
+    is_buy_array = df_portfolio.apply(lambda x: x["buy_sell_label"]>=7, axis=1) - 0
+    is_buy_array = is_buy_array.values
+    close_array =  df_portfolio["close"].values
+    date_array = df_portfolio["date"].values
+    profit, sharpe, annual_return, max_drawdown, df_profits = back_test(is_buy_array, close_array, date_array)
+    df_profits = df_profits[["date", "rate"]]
+    df_profits = df_profits.reset_index(drop=True)
+    df_portfolio = df_portfolio.merge(df_profits, on="date")
+
+    options = list(range(int(min_date[:4]), int(max_date[:4]) + 1))[::-1]
+    options = [str(ele) for ele in options]
+    options = ['all'] + options
+
+    st.markdown("### 收益曲线")
+    select_year = st.selectbox(label='年份', options=options, key=1)
+    if select_year != 'all':
+        df_portfolio = df_portfolio[df_portfolio.date.map(lambda x: x[:4] == select_year)]
+
+    df_portfolio.index = pd.to_datetime(df_portfolio['date'])
+    df_portfolio['rate'] = df_portfolio['rate'] / df_portfolio['rate'].iloc[0]
+    df_portfolio["profit"] = df_portfolio["rate"] / df_portfolio["rate"].shift() - 1
+
+    df_portfolio.index = pd.to_datetime(df_portfolio['date'])
+    df_portfolio['rate'] = df_portfolio['rate'] / df_portfolio['rate'].iloc[0]
+    df_portfolio["profit"] = df_portfolio["rate"] / df_portfolio["rate"].shift() - 1
+
+    accu_returns, annu_returns, max_drawdown, sharpe = calc_indicators(df_portfolio['profit'])
+    accu_returns = round(accu_returns, 3)
+    annu_returns = round(annu_returns, 3)
+    max_drawdown = round(max_drawdown, 3)
+    sharpe = round(sharpe, 3)
+
+    min_values = [df_portfolio["rate"].min()]
+    max_values = [df_portfolio["rate"].max()]
+    min_value = round(min(min_values) * 0.98, 2)
+    max_value = round(max(max_values) * 1.02, 2)
+
+    options = {
+        "xAxis": {
+            "type": "category",
+            "data": df_portfolio['date'].tolist(),
+        },
+        "yAxis": {"type": "value", 'min': min_value, 'max': max_value},
+        "series": [
+            {"data": df_portfolio['rate'].tolist(), "type": "line"}],
+        "tooltip": {
+            'trigger': 'axis',
+            'backgroundColor': 'rgba(32, 33, 36,.7)',
+            'borderColor': 'rgba(32, 33, 36,0.20)',
+            'borderWidth': 1,
+            'textStyle': {
+                'color': '#fff',
+                'fontSize': '12'
+            },
+            'axisPointer': {
+                'type': 'cross',
+                'label': {
+                    'backgroundColor': '#6a7985'
+                }
+            },
+        },
+        "title": {
+            'text': f'''累计收益: {accu_returns}\n年化收益: {annu_returns}\n最大回撤:{max_drawdown}\n夏普比:{sharpe}''',
+            'right': 'left',
+            'top': '0px',
+        }
+    }
+    st_echarts(options=options, height=400)
+
+    df_portfolio = df_portfolio.reset_index(drop=True)
+    df_portfolio["profit_str"] = df_portfolio["profit"].map(lambda x: str(round(100 * x, 3)) + "%")
+    df_portfolio_daily = df_portfolio[['date', 'profit_str', "profit"]]
+    df_portfolio_daily = df_portfolio_daily.sort_values("date", ascending=False)
+    df_portfolio_daily = df_portfolio_daily.head(100)
+    df_portfolio_month = df_portfolio
+    df_portfolio_month["月份"] = df_portfolio["date"].map(lambda x: str(x[:7]))
+    df_portfolio_month = df_portfolio_month.groupby("月份", as_index=False)["profit"].sum()
+    df_portfolio_month["收益率"] = df_portfolio_month["profit"].map(lambda x: str(round(100 * x, 3)) + "%")
+    df_portfolio_month = df_portfolio_month.sort_values(by="月份", ascending=False)
+
+    month, year, day = st.tabs(['每月收益率分析', '每年收益率分析', '每日收益率分析'])
+    with month:
+        st.markdown("### 每月收益率分析")
+        st.bar_chart(df_portfolio_month.head(12), x='月份', y='profit')
+        st.dataframe(df_portfolio_month[['月份', '收益率']], hide_index=True, width=width, height=300)
+
+    with year:
+        df_year = mysql_conn.query(
+            "select date, value from etf.ads_etf_portfolio_profit_summary where date_type='year' ")
+        df_year = df_year.sort_values(by='date', ascending=False)
+        df_year["收益率"] = df_year.value.map(lambda x: str(round(x, 2)) + "%")
+        st.markdown("### 每月收益率分析")
+        st.bar_chart(df_year.head(12), x='date', y='value')
+        st.dataframe(df_year[['date', '收益率']], hide_index=True, width=width, height=300)
+
+    with day:
+        st.markdown("### 每日收益率分析")
+        st.bar_chart(df_portfolio_daily.head(12), x='date', y='profit')
+        st.dataframe(df_portfolio_daily[['date', 'profit_str']], hide_index=True, width=width, height=300)
 
 
-def macro_strategy():
-    sql = '''
-    select date, close, 
-		   avg(close) over (order by date rows 4 preceding) avg5,
-			 avg(close) over (order by date rows 19 preceding) avg20
-    from stock.micro_index t
-    order by date desc
-    limit 100
-    '''
-    df_macro = mysql_conn.query(sql, ttl=0)
-    df_macro['is_bull'] = df_macro.apply(lambda x: int(x['avg5'] > x['avg20']), axis=1)
-    df_macro = df_macro[['date', 'is_bull']]
-    st.dataframe(df_macro, hide_index=True, width=width, height=1000)
+rank_st, rotation, portfolio, hot_new = st.tabs(["组合择时", "纳斯达克", "组合投资", "投资热点"])
 
-
-nn, portfolio, hot_new = st.tabs(["轮动策略", "组合投资", "投资热点"])
-
-with nn:
+with rank_st:
+    rank_portfolio_strategy()
+    
+with rotation:
     rotation_strategy()
 
 with portfolio:
@@ -253,9 +399,3 @@ with portfolio:
 
 with hot_new:
     get_hot_invest()
-
-
-
-
-# with board_strategy_ui:
-#     board_strategy()
