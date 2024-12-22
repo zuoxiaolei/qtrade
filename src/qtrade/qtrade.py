3e568c0947c31d9cb2df95cf5e81f7f4675d6cf1
Your Name <you@example.com>
2024-12-22 19:12:15 +0800

    fix qtrade

--- src/qtrade/qtrade.py
+++ src/qtrade/qtrade.py
@@ -3,5 +3,6 @@ import streamlit as st
 pg = st.navigation([st.Page("pages/qtrade_forex.py", title="外汇组合"),
                     st.Page("pages/qtrade_btc.py", title="btc策略"),
                     st.Page("pages/qtrade_etf.py", title="ETF组合"),
+                    st.Page("pages/qtrade_matchless.py", title="无双策略")
                     ])
 pg.run()
